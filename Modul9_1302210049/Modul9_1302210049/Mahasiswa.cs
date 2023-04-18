0af3520bb967a96655502c30cebc7c9f66d1cd8f
ecospheree <121998521+ecospheree@users.noreply.github.com>
2023-04-18 17:21:18 +0700

    menambahkan API mahasiswa

---
+++ Modul9_1302210049/Modul9_1302210049/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿namespace Modul9_1302210049
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public List<string> course { get; set; }
+        public int year { get; set; }
+        public Mahasiswa(string nama, string nim, List<string> course, int year) 
+        {
+            this.nama = nama;
+            this.nim = nim;
+            this.course = course;
+            this.year = year;
+        }
+    }
+}
---
+++ Modul9_1302210049/Modul9_1302210049/MahasiswaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modul9_1302210049;
+
+namespace Modul9_1302210049
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController
+    {
+        private static List<Mahasiswa> mahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa("Nabiel Prayoga Budiana", "1302210049", new List<string> { "KPL", "BASDAT" }, 2021),
+            new Mahasiswa("Walid Hanif Ataullah", "1302213120", new List<string> { "KPL", "BASDAT" }, 2021),
+            new Mahasiswa("Muhammad Raffa Zuhayr", "1302210068", new List<string> { "KPL", "BASDAT" }, 2021),
+            new Mahasiswa("Muhammad Yaasin Rafi", "1302210055", new List<string> { "KPL", "BASDAT" }, 2021),
+            new Mahasiswa("Sylvana Rheina Kharmeliawaty", "1302210017", new List<string> { "KPL", "BASDAT" }, 2021)
+        };
+        //membuat get di controller
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswa;
+        }
+
+        //menambahkan object 
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswa.Add(value);
+        }
+
+        //get sesuia index
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswa[id];
+        }
+
+        //untuk mengahapus index
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mahasiswa.RemoveAt(id);
+        }
+    }
+}
+
+
+
+            
